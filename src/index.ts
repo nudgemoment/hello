3946ef65278c1f19d1554108a9c4c82f501e6cbb
jibjiby <my01sun@gmail.com>
2025-01-02 19:37:41 +0900

    chore: global name 수정

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// window.NudgeMoment = Object.assign(window.NudgeMoment || {}, {
+//   // 새로운 속성/메서드
+// })
+
 export const sayHello = (name: string = 'World'): string => {
   return `Hello, ${name}!`;
 };
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['iife'], // IIFE 형식으로 변경
-  globalName: 'HelloPackage', // 전역 변수 이름
+  globalName: 'NudgeMoment', // 전역 변수 이름
   dts: true,
   splitting: false,
   clean: true,
